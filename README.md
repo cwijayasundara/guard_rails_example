08448fac46883b13149b55dc9ec586bc397d539a
“cwijayasundara” <cwijayasundara@gmail.com>
2024-11-11 16:56:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Follow the instructions in the page below:
+
+https://www.guardrailsai.com/docs/integrations/langchain
---
+++ app.py
@@ -0,0 +1,22 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from guardrails import Guard
+from guardrails.hub import CompetitorCheck
+
+model = ChatOpenAI(model="gpt-4o-mini")
+
+competitors_list = ["delta", "american airlines", "united"]
+
+guard = Guard().use_many(
+    CompetitorCheck(competitors=competitors_list, on_fail="fix")
+)
+
+prompt = ChatPromptTemplate.from_template("Answer this question {question}")
+
+output_parser = StrOutputParser()
+
+chain = prompt | model | guard.to_runnable() | output_parser
+
+result = chain.invoke({"question": "What are the top five airlines for domestic travel in the US?"})
+print(result)
